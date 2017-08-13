914060e481a102b0acb07669bb39bbfdb97af215
Mason Shin <mshin@ten-x.com>
2017-08-12 20:59:42 -0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Elements
+export * as Button from './src/components/elements/Button'
+export * as Input from './src/components/elements/Input'
+
+// Layouts
+export * as Grid from './src/components/layouts/Grid'
+export * as CenteredRow from './src/components/layouts/Grid/CenteredRow'
+export * as Column from './src/components/layouts/Grid/Column'
+export * as Row from './src/components/layouts/Grid/Row'
