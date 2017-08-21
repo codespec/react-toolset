d60f79532aa4275f76eafb03a5eb6247e78c2a1a
Mason Shin <mshin@ten-x.com>
2017-08-20 22:56:02 -0700

    Enhance Button component to support more color variation and style

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -5,24 +5,27 @@ import '../../../styles/bumblebee.scss'
 import { button as buttonCls } from '../../../styles'
 const cls = cx.bind(buttonCls)
 
-const Button = ({role, size, tag, isStretched, isSelected, link, className, children, ...props}) => {
+const Button = ({role, size, tag, isStretched, isSelected, link, isFlat, className, children, ...props}) => {
   const tagName = link ? 'a' : (tag || 'button')
+  const style = isFlat ? 'flat' : 'rounded'
 
   return React.createElement(tagName, {
     href: (link || null),
-    className: cls('Button', `Button-${role}`, `Button-${size}`, { 'Button-is-stretched': isStretched }, { 'Button-is-selected': isSelected }, className),
+    className: cls('Button', `Button-${role}`, `Button-${size}`, `Button-is-${style}`, { 'Button-is-stretched': isStretched }, { 'Button-is-selected': isSelected }, className),
     ...props
   }, children)
 }
 
 Button.propTypes = {
-  role: PropTypes.oneOf(['primary', 'secondary', 'default']),
+  role: PropTypes.oneOf(['primary', 'secondary', 'success', 'warning', 'error', 'info', 'default']),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   tag: PropTypes.oneOf(['button', 'div', 'a']),
   /** Whether the button takes up the full width of its container */
   isStretched: PropTypes.bool,
   /** Whether to apply a depressed styling to the button to make it look on or selected */
   isSelected: PropTypes.bool,
+  /** Whether flat button or rounded button */
+  isFlat: PropTypes.bool,
   /** Value of href to apply to the button if and `<a>` element is being used for the button */
   link: PropTypes.string,
   /** Name of extra class to apply to the root element */
@@ -35,7 +38,8 @@ Button.defaultProps = {
   role: 'default',
   size: 'medium',
   tag: 'button',
-  isStretched: false
+  isStretched: false,
+  isFlat: false
 }
 
 export default Button
