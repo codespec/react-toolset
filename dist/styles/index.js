45acf274433d7d9f3dea68e86dfa37f5c671865d
Mason Shin <js.mason.shin@gmail.com>
2017-08-31 11:52:44 -0700

    Fix dist (#19)
    * Fix for missing javascript files in dist
    * Bump up the version

---
+++ dist/styles/index.js
@@ -0,0 +1,4 @@
+export { default as button } from './buttons.css';
+export { default as form } from './form.css';
+export { default as link } from './links.css';
+export { default as util } from './utils.css';
