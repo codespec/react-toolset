b1630d64e59bbfe3a7b71ac3981fc04140a8cd08
RaulZhao <raul.zhao@spirent.com>
2017-09-25 10:26:11 -0700

    Update by Raul

--- __tests__/components/Pagination/pagination.test.js
+++ __tests__/components/Pagination/pagination.test.js
@@ -17,24 +17,25 @@ describe('<Pagination />', () => {
   })
 
   it('renders Pagination component with specific link items', () => {
-    const maxPaginationSize = 9
-    const component = mount(<Pagination {...props} maxPaginationSize={maxPaginationSize} />)
+    const newProps = Object.assign({}, props, { maxPaginationSize: 9 })
+    const component = mount(<Pagination {...newProps} />)
 
-    expect(component.find('.PaginationLink').length).toEqual(maxPaginationSize)
+    expect(component.find('.PaginationLink').length).toEqual(newProps.maxPaginationSize)
   })
 
   it('renders Pagination component with clicking first link', () => {
-    const currentPage = 2
-    const component = mount(<Pagination {...props} currentPage={currentPage} />)
+    const newProps = Object.assign({}, props, { currentPage: 2 })
+    const component = mount(<Pagination {...newProps} />)
 
     component.find('.PaginationLink').first().find('a').simulate('click')
-    expect(props.onChange.called)
+    expect(newProps.onChange.called)
     expect(component.find('.Pagination__caret--left').find('span').length).toEqual(1)
   })
 
   it('renders Pagination component with clicking last link', () => {
     const currentPage = Math.ceil(props.totalItems / props.itemsPerPage) - 1
-    const component = mount(<Pagination {...props} currentPage={currentPage}/>)
+    const newProps = Object.assign({}, props, { currentPage })
+    const component = mount(<Pagination {...newProps} />)
 
     component.find('.PaginationLink').last().find('a').simulate('click')
     expect(component.find('.Pagination__caret--right').find('span').length).toEqual(1)
