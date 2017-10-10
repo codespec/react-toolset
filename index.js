65ca5d261ce5368e755dc395593a9d9bf5ff831a
Mason Shin <mshin@ten-x.com>
2017-10-09 22:11:12 -0700

    Fix typo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import Checkbox from './dist/elements/Checkbox'
 // components
 import Breadcrumb from './dist/components/Breadcrumb'
 import Progressbar from './dist/components/Progressbar'
-import Pagination from './dist/componnets/Pagination'
+import Pagination from './dist/components/Pagination'
 
 // Layouts
 import Column from './dist/layouts/Grid/Column'
